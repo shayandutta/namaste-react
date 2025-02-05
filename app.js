9fde03c8c3eab9610301062cb4f64fad3975d0fc
shayandutta <shayandutta23@gmail.com>
2025-02-06 02:33:43 +0530

    added components composition

--- app.js
+++ app.js
@@ -1,56 +1,42 @@
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 
 
-// const heading = React.createElement("h1", { id: "heading" }, "hello dojo!"); //heading -> react element & react element is nothing but a javascript object
-// console.log(heading);//we find javascript object in the console
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(heading);//this functions takes the react elememt (javascript object) and renders it to the dom(as an html tag that the browser understands)
-//this render method converts the object to html tag and renders it to the dom
-
-//nested html (parent-child relationship)
-
-/* 
-// nested html
-
-
-
-<div id="parent">
-    <div id="child">
-        <h1>I'm h1 tag</h1>
-        <h2>i'm h2 tag</h2>
-    </div>
-    <div id="child2">
-        <h1>i'm h1 tag</h1>
-        <h2>i'm h2 tag</h2>
-    </div>
-</div> 
+/*
+*Header
+  - Logo
+  - Nav items
+*Body
+  - Search
+  - RestaurantContainer
+    - RestaurantCard
+*Footer
+  - CopyRight
+  - Links
+  - Address
+  - Contact
 */
 
+const Title = () => (
+    <h1 className="heading">Namaste reaact functional ccomponent</h1>
+);
 
-// nested react
-//nested react
 
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("h1", { id: "child" }, [
-    React.createElement("h1", {}, "this is namaste react"),
-    React.createElement("h2", {}, "this is namaste react"),
-  ]),
-  React.createElement("h1", { id: "child2" }, [
-    React.createElement("h1", {}, "i am an h1 tag"),
-    React.createElement("h2", {}, "i am an h2 element"),
-  ]),
-]);
+
+const HeaadingComponents = () => {
+  return (
+    <div id="container">
+      <h1>{console.log("hello")}</h1>
+      {Title()} {/*can call functions also inside functional components*/}
+      {/* <Title/> */}
+      <h1 className="heading">Namaste react functional component</h1>
+    </div>
+  )
+}
+
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
 
-//but this is so difficult to write
-//so we use jsx
-//jsx is a syntax extension for javascript and it looks like html but it is not html
-//jsx is a syntactic sugar for react.createElement method
-//but react.createElement is more powerful than jsx and is the core of react 
-//jsx is easier to write and understand 
-//jsx is compiled to react.createElement method by babel (a javascript compiler) 
-//jsx is not a 
+
+root.render(<HeaadingComponents/>)
